31150185515a93bedbe41f4cc361018757ea66d9
Nate Reiners <nate.reiners@gmail.com>
2017-03-04 19:45:46 -0800

    complete calculator

--- calculator/calculator.jsx
+++ calculator/calculator.jsx
@@ -3,15 +3,67 @@ import React from 'react';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    // your code here
+    this.state = {num1: "", num2: "", result: 0};
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.clear = this.clear.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.divide = this.divide.bind(this);
+    this.multiply = this.multiply.bind(this);
   }
 
-  // your code here
+  setNum1(e) {
+    e.preventDefault();
+    const n1 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num1: n1 });
+  }
+
+  setNum2(e) {
+    e.preventDefault();
+    const n2 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num2: n2 });
+  }
+
+  clear(e) {
+    e.preventDefault();
+    this.setState({ num1: "", num2: "", result: 0 });
+  }
+
+  add(e) {
+    e.preventDefault();
+    const result = this.state.num1 + this.state.num2;
+    this.setState({ result });
+  }
+  subtract(e) {
+    e.preventDefault();
+    const result = this.state.num1 - this.state.num2;
+    this.setState({ result });
+  }
+  divide(e) {
+    e.preventDefault();
+    const result = this.state.num1 / this.state.num2;
+    this.setState({ result });
+  }
+  multiply(e) {
+    e.preventDefault();
+    const result = this.state.num1 * this.state.num2;
+    this.setState({ result });
+  }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1> // replace this with your code
+        <input onChange={this.setNum1} value={this.state.num1} />
+        <input onChange={this.setNum2} value={this.state.num2} />
+        <button onClick={this.clear}>Clear</button>
+        <br/>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.subtract}>-</button>
+        <button onClick={this.divide}>/</button>
+        <button onClick={this.multiply}>*</button>
+        <br/>
+        <h1>Result: {this.state.result}</h1>
       </div>
     );
   }
